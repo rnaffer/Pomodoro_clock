474afe762216c14afb8b2ea6c360ed6fc7e8a673
RNaffer <reyesnaffer@gmail.com>
2015-10-03 22:26:55 -0430

    first commit + circle bar done

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+var transform_styles = ['-webkit-transform',
+                        '-ms-transform',
+                        'transform'];
+window.randomize = function() {
+	var rotation = Math.floor(Math.random() * 180);
+	var fill_rotation = rotation;
+	var fix_rotation = rotation * 2;
+
+	for(i in transform_styles) {
+		$('.circle .fill, .circle .mask.full').css(transform_styles[i],
+			'rotate(' + fill_rotation + 'deg)');
+
+		$('.circle .fill.fix').css(transform_styles[i],
+                             'rotate(' + fix_rotation + 'deg)');
+	}
+}                    
